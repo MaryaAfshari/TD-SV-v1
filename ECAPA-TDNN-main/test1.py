2cb09d69fa3aec7e45a275b7fe219c7da7dd0904
afshari-maryam <afshari1431@gmail.com>
2024-04-04 17:38:27 +0300

    first change

--- ECAPA-TDNN-main/test1.py
+++ ECAPA-TDNN-main/test1.py
@@ -1,3 +1,4 @@
 #In the name of GOD
 #30-march-2023 - shanbe-- 11-farvardin-1403
-print("Hello Practical Speech word date :31-March-2024")
+#4-4-2024 - panjshanbe - 16-farvardin-1403
+print("Hello Practical Speech word date :4-April-2024")
