a8189546732031d2abf2efdb56467ab8e789639b
afshari-maryam <afshari1431@gmail.com>
2024-04-15 18:59:52 +0300

    print ha

--- ECAPA-TDNN-main/test1.py
+++ ECAPA-TDNN-main/test1.py
@@ -1,4 +1,10 @@
 #In the name of GOD
 #30-march-2023 - shanbe-- 11-farvardin-1403
 #4-4-2024 - panjshanbe - 16-farvardin-1403
+#7-4-2024 - Yekshanbe -
+#8-4-2024 - Doshanbe -
+#10-4-2024- Chaharshanbe-22-Farvardin
+#15-4-2024- Dooshanbe - 27-farvardin-1403
 print("Hello Practical Speech word date :4-April-2024")
+print("Hello Practical Speech word date :10-April-2024")
+print("Hello Practical Speech word date :15-April-2024")
