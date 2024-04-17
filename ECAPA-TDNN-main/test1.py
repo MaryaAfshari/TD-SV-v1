54524770d8ff142a7d18ce91e5df95f5731c890b
afshari-maryam <afshari1431@gmail.com>
2024-04-17 20:27:59 +0300

    reading from ndx file of Target Correct also

--- ECAPA-TDNN-main/test1.py
+++ ECAPA-TDNN-main/test1.py
@@ -14,7 +14,17 @@ print("Hello Practical Speech word date :17-April-2024")
 # trn address file: /mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk/3-sess
 # ndx address file: /mnt/disk1/data/DeepMine/key/text-dependent/ndx/ENG/male/100-spk/3-sess
 trn_dev_addr = "../../../../../mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk/3-sess/dev.trn"
+ndx_dev_TC_addr = "../../../../../mnt/disk1/data/DeepMine/key/text-dependent/ndx/ENG/male/100-spk/3-sess/dev_TC.ndx" #Target Correct
 with open(trn_dev_addr, 'r') as file:
+    line_count = 0
+    for line in file:
+        print(line)
+        line_count += 1
+        if line_count == 2:
+            break
+
+
+with open(ndx_dev_TC_addr, 'r') as file:
     line_count = 0
     for line in file:
         print(line)
