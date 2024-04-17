676c7785962f4d5a28536cbb29e6dc7f0fe30cc2
afshari-maryam <afshari1431@gmail.com>
2024-04-17 20:18:01 +0300

    add dots in address

--- ECAPA-TDNN-main/test1.py
+++ ECAPA-TDNN-main/test1.py
@@ -13,7 +13,7 @@ print("Hello Practical Speech word date :17-April-2024")
 #code address : /code1-29-3-2024/TD-SV-v1/ECAPA-TDNN-main
 # trn address file: /mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk/3-sess
 # ndx address file: /mnt/disk1/data/DeepMine/key/text-dependent/ndx/ENG/male/100-spk/3-sess
-trn_dev_addr = "/mnt/disk1/data/DeepMine/key/text-dependent/ndx/ENG/male/100-spk/3-sess/dev.trn"
+trn_dev_addr = "../../../../../mnt/disk1/data/DeepMine/key/text-dependent/ndx/ENG/male/100-spk/3-sess/dev.trn"
 with open(trn_dev_addr, 'r') as file:
     line_count = 0
     for line in file:
