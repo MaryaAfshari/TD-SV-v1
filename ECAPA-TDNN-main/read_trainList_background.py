269a62d1f91b127c62d7c922739394fec55d3f89
afshari-maryam <afshari1431@gmail.com>
2024-05-06 13:16:13 +0300

    trainlist was writed to another category

--- ECAPA-TDNN-main/read_trainList_background.py
+++ ECAPA-TDNN-main/read_trainList_background.py
@@ -1,6 +1,7 @@
 #                                           In the name of GOD
 #Author: Maryam Afshari
-#05-05-2024-Yekshanbe 16-Ordibehesht-1403  ----> 
+#05-05-2024-Yekshanbe 16-Ordibehesht-1403  ----> initial this code
+#06-05-2024-Doshanbe 17-Ordibehesht-1403 ---> write trainlist in a code
 import numpy as np
 #/mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk
 background_addr = "../../../../../mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk/background.trn"
@@ -10,8 +11,8 @@ input_file_path =  background_addr
 output_file_path = '../../../ResultFile1-24-4-2024/train_labels_background.txt'
 
 with open(input_file_path, 'r') as file:
-    # lines = file.readlines()[:4]  # Read only the first 4 lines
-    lines = file.readlines()[:20] 
+    # lines = file.readlines()[:20]  # Read only the first 4 lines
+    lines = file.readlines()
 
 number_of_lines = len(lines)
 print("The file contains " + str(number_of_lines) + " lines in " + input_file_path + ".")
@@ -23,4 +24,26 @@ for line in lines:
     speaker_id = parts[1]
     phrase_id = parts[2]
     label = '1'
-    print(f" {speaker_id} {utt_path} {label} {phrase_id}\n")
+    print(f" {speaker_id} {utt_path} {phrase_id} {label}\n")
+
+# Open the output file to write the results
+with open(output_file_path, 'w') as output_file:
+        
+        for line in lines:
+            parts = line.strip().split()
+            utt_path = parts[0]
+            speaker_id = parts[1]
+            phrase_id = parts[2]
+            label = '1'
+            output_file.write(f"{speaker_id} {utt_path} {phrase_id} {label}\n")
+
+print("Data for the lines has been written to train_labels.txt successfully.")
+
+#check the number of lines written in 
+line_Writing_count = 0
+with open(output_file_path, 'r') as file:
+    for line in file:
+        line_Writing_count += 1
+
+print(f"The file contains {number_of_lines} lines in {input_file_path}.")
+print(f"The file contains {line_Writing_count} lines in {output_file_path}.")
