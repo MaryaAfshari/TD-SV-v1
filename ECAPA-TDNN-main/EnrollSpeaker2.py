29c86c16c820d10f0066981771c79a195f27fddf
afshari-maryam <afshari1431@gmail.com>
2024-05-16 12:08:58 +0300

    check sample rate of different model in wavs

--- ECAPA-TDNN-main/EnrollSpeaker2.py
+++ ECAPA-TDNN-main/EnrollSpeaker2.py
@@ -1,5 +1,5 @@
 #"16-05-2024" 27 Ordibehesht 1403 Panjshanbe "Ya Malekolhagholmobin"
-import os
+import os, soundfile
 
 
 def parse_eval_file(file_path):
@@ -31,12 +31,21 @@ def process_wav_files_by_speaker(speaker_data, speaker_id):
         total_wav_files = 0
         for phrase_id, model_index, wav_files in speaker_data[speaker_id]:
             for wav_file in wav_files:
-                print(f'Processing {wav_file} for speaker {speaker_id}, phrase {phrase_id}, model {model_index}')
+                audio, sr = load_audio(wav_file)
+                #print(f'Processing {wav_file} for speaker {speaker_id}, phrase {phrase_id}, model {model_index}')
+                print(f'Processing {wav_file} for speaker {speaker_id}, phrase {phrase_id}, model {model_index}, sample rate: {sr}')
                 total_wav_files += 1 
                 # Implement your processing logic here
         print(f'Total number of wav files for speaker {speaker_id}: {total_wav_files}')
     else:
         print(f'No data found for speaker {speaker_id}')
+
+
+def load_audio(audio_path):
+    import librosa
+    audio, sr = librosa.load(audio_path, sr=16000)
+    #audio, _  = soundfile.read(os.path.join(eval_path, file))
+    return audio, sr
 
 dev_input_file = "../../../../../mnt/disk1/data/DeepMine/key/text-dependent/trn/ENG/male/100-spk/3-sess/dev.trn"
 dev_output_file = '../../../ResultFile1-24-4-2024/dev_speaker_data.txt'
